8853f8f8ca80e16065c0e9189c5da53d6b4d67c5
Oskar Eriksson <oskare90@gmail.com>
2014-03-03 15:08:27 +0100

    Simple ExecuteSeq() implementation.

--- SQLTestFramework/Framework/TestExecutor.cs
+++ SQLTestFramework/Framework/TestExecutor.cs
@@ -3,23 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Starcounter;
 
 namespace SQLTestFramework.Framework
 {
     /// <summary>
     /// Execute test cases and store results
     /// </summary>
+    // TODO: Read/Write internal parameters + parameter validator
     class TestExecutor: ITestExecutor
     {
+        const int ExecutionIterations = 2;
 
+        /// <summary>
+        /// Execute tests in parallel and store the results
+        /// </summary>
+        /// <param name="tests">List of tests to be executed.</param>
         public void ExecutePar(List<SQLTestCase> tests)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Execute tests sequentially and store the results.
+        /// </summary>
+        /// <param name="tests">List of tests to be executed.</param>
         public void ExecuteSeq(List<SQLTestCase> tests)
         {
-            throw new NotImplementedException();
+            Db.Transaction(delegate
+            {
+                // temporary loggging
+                for (int i = 0; i < ExecutionIterations; i++)
+                {
+                    Console.WriteLine("Starting execution run " + i);
+                    foreach (SQLTestCase test in tests){
+                        test.Execute(Convert.ToBoolean(i));
+                    }
+                    Console.WriteLine("Execution run " + i + " finished");
+                }
+            });
         }
     }
 }
