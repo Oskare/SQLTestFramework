8771a45636203bcacf6fe300305a892b5dda8bec
Oskar Eriksson <oskare90@gmail.com>
2014-03-04 14:50:18 +0100

    Type update

--- SQLTestFramework/Framework/TestExecutor.cs
+++ SQLTestFramework/Framework/TestExecutor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Starcounter;
 
 namespace SQLTestFramework.Framework
@@ -13,13 +13,13 @@ namespace SQLTestFramework.Framework
     // TODO: Read/Write internal parameters + parameter validator
     class TestExecutor: ITestExecutor
     {
-        const int ExecutionIterations = 3;
+        const int ExecutionIterations = 2;
 
         /// <summary>
         /// Execute tests in parallel and store the results
         /// </summary>
         /// <param name="tests">List of tests to be executed.</param>
-        public void ExecutePar(List<SQLTestCase> tests)
+        public void ExecutePar(List<ISQLTestCase> tests)
         {
             throw new NotImplementedException();
         }
@@ -28,7 +28,7 @@ namespace SQLTestFramework.Framework
         /// Execute tests sequentially and store the results.
         /// </summary>
         /// <param name="tests">List of tests to be executed.</param>
-        public void ExecuteSeq(List<SQLTestCase> tests)
+        public void ExecuteSeq(List<ISQLTestCase> tests)
         {
             Db.Transaction(delegate
             {
@@ -36,7 +36,7 @@ namespace SQLTestFramework.Framework
                 for (int i = 0; i < ExecutionIterations; i++)
                 {
                     Console.WriteLine("Starting execution run " + i);
-                    foreach (SQLTestCase test in tests){
+                    foreach (ISQLTestCase test in tests){
                         test.Execute();
                     }
                     Console.WriteLine("Execution run " + i + " finished");
